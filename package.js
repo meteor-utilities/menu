2817b586e402daf2381b9c50348fda952472e7b7
Sacha Greif <sacha357@gmail.com>
2015-10-22 16:51:04 +0900

    more tweaks

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "utilities:menu",
   summary: "Telescope menu component package",
-  version: "0.1.3",
+  version: "0.1.5",
   git: "https://github.com/meteor-utilities/menu"
 });
 
